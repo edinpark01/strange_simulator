7af3e6012303326b4bf37f2d5b5563570f27e1cb
edinpark <tonaco.braulio@gmail.com>
2019-05-10 10:00:37 -0400

    TODOs +get_arguments function

--- python/simulator.py
+++ python/simulator.py
@@ -1,50 +1,66 @@
 import threading
 import os
 import boto3
-import json
 import time
+import sys
 
 
-def upload(local, remote, fn, reg):
-    with open('aws_info.json') as json_file:
-        data = json.load(json_file)
-        error = False
-
-        try:
-            session = boto3.Session(
-                aws_access_key_id=data['aws_access_key_id'],
-                aws_secret_access_key=data['aws_secret_access_key'],
-                region_name=reg
-            )
-        except:
-            error = True
-            print("EXCEPTION:\tError while initializing Session")
-            print("\t\t\tRegion => {}".format(data["region_name"]))
-
-        try:
-            client = session.client(service_name='s3')
-        except:
-            error = True
-            print("EXCEPTION:\tError while initializing client")
-
-        try:
-            print("INFO:\t\tFile: {}\t\tUploading...".format(fn))
-            start = time.time()
-            client.upload_file(local, data['bucket_name'], remote)
-            end = time.time()
-        except:
-            error = True
-            print("EXCEPTION:\tError while uploading file: {}".format(fn))
-            print("\t\t=> Bucket: {}".format(data['bucket_name']))
-
-        if not error:
-            print("INFO:\t\tFile: {}\t\tUpload Time: {} milliseconds".format(fn, (int(round((end - start) * 1000)))))
-
-        return local
+def get_arguments():
+    # TODO: Write UnitTest
+    # TODO: Write DocStr
+    if len(sys.argv) is not 5:
+        print("EXCEPTION:\tError while reading arguments | expected 3")
+        return None
+    else:
+        return {
+            "access_key": sys.argv[1],
+            "secret_key": sys.argv[2],
+            "region": sys.argv[3],
+            "bucket_name": sys.argv[4]
+        }
 
 
-def run(region):
-    sample_dir = "sample_data"
+def upload(ak, sk, rn, bk, lp, fn):
+    error = False
+    elapsed_time = -1
+    try:
+        session = boto3.Session(
+            aws_access_key_id=ak,
+            aws_secret_access_key=sk,
+            region_name=rn
+        )
+    except:
+        error = True
+        print("EXCEPTION:\tError while initializing Session")
+        print("\t\t\tRegion => {}".format(rn))
+
+    try:
+        client = session.client(service_name='s3')
+    except:
+        error = True
+        print("EXCEPTION:\tError while initializing client")
+
+    try:
+        print("INFO:\t\tFile: {}\t\tUploading...".format(fn))
+        start = time.time()
+        client.upload_file(lp, bk, fn)
+        elapsed_time = time.time() - start
+    except:
+        error = True
+        print("EXCEPTION:\tError while uploading file: {}".format(fn))
+        print("\t\t=> Bucket: {}".format(bk))
+
+    if not error:
+        millisec = int(round(elapsed_time * 1000))
+        print("INFO:\t\tFile: {}\t\tUpload Time: {} milliseconds".format(fn, millisec))
+
+
+def run(data: dict):
+    """
+    TODO: Write doc string
+    TODO: Write UnitTest
+    """
+    sample_dir = "./sample_data"
 
     if not os.path.isdir(sample_dir):
         print("EXCEPTION:\t\tSample Data directory does not exist")
@@ -55,16 +71,21 @@ def run(region):
                 print("EXCEPTION:\t\tCould not read any files")
 
             for file_name in files:
-                local_path = os.path.join(root, file_name)
-                s3_path = os.path.relpath(local_path, ".")
-
-                threading.Thread(target=upload, args=(local_path, s3_path, file_name, region)).start()
+                threading.Thread(
+                    target=upload,
+                    name=file_name,
+                    args=(
+                        data['access_key'],
+                        data['secret_key'],
+                        data['region'],
+                        data['bucket_name'],
+                        os.path.join(root, file_name),
+                        file_name
+                    )).start()
 
 
 if __name__ == "__main__":
-    import sys
+    args = get_arguments()
 
-    if len(sys.argv) != 2:
-        print("EXCEPTION:\t\tMissing EC2 instance region")
-    else:
-        run(sys.argv[1])
+    if args is not None:
+        run(args)
