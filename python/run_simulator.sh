b58436d53725d4caacb0747cdcf068a14f83d61f
edinpark <tonaco.braulio@gmail.com>
2019-05-13 20:35:49 -0400

    using environment variables instead

--- python/run_simulator.sh
+++ python/run_simulator.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-access_key=$1
-secret_key=$2
-region=$3
-bucket_name=$4
-
 source /simulator/venv/bin/activate
-python /simulator/run_simulator.sh ${access_key} ${secret_key} ${region} ${bucket_name}
+python /simulator/run_simulator.sh ${ACCESS_KEY} ${SECRET_KEY} ${REGION} ${BUCKET_NAME}
 deactivate
 
