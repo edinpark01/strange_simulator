2c421c639330fdb1e3a967306818bc81dd10b787
edinpark <tonaco.braulio@gmail.com>
2019-05-13 22:23:54 -0400

    removed sudo

--- python/run_simulator.sh
+++ python/run_simulator.sh
@@ -1,3 +1,3 @@
 source /simulator/venv/bin/activate
-sudo python /simulator/simulator.py ${ACCESS_KEY} ${SECRET_KEY} ${REGION} ${BUCKET_NAME}
+python /simulator/simulator.py ${ACCESS_KEY} ${SECRET_KEY} ${REGION} ${BUCKET_NAME}
 deactivate
